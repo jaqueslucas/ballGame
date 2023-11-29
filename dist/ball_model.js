ff9804f08670e8d65da883f444a8a805b5657eb3
jaqueslucas <lucassouza2347@hotmail.com>
2023-11-29 16:07:24 -0300

    versao final 1.0 do jogo

--- dist/ball_model.js
+++ dist/ball_model.js
@@ -1,31 +1,22 @@
-let my_screen = document.getElementById('game_screen') as HTMLCanvasElement;
-const context = my_screen.getContext('2d') as CanvasRenderingContext2D;
-
+"use strict";
+let my_screen = document.getElementById('game_screen');
+const context = my_screen.getContext('2d');
 let rectY = my_screen.height / 2; // Posição inicial da base retangular
-let circles: {
-    x: number;y: number;radius: number
-} [] = []; // Armazenar os círculos
-
+let circles = []; // Armazenar os círculos
 let gameRunning = false; // Variável para controlar o estado do jogo
 let score = 0; // Variável para armazenar a pontuação
-
-let soundtrack: HTMLAudioElement | null = null; // Variável para armazenar a música de fundo
-
-
+let soundtrack = null; // Variável para armazenar a música de fundo
 // Função para desenhar a base retangular móvel
 function drawRect() {
     context.fillStyle = 'red';
     context.fillRect(my_screen.width - 20, rectY, 20, 40); // Posição e tamanho da base retangular
 }
-
-
 // Função para verificar a colisão entre os círculos e a base retangular
 function checkCollision() {
     for (let i = 0; i < circles.length; i++) {
         const circle = circles[i];
         // Verificar se houve colisão com a base retangular
-        if (
-            circle.x + circle.radius >= my_screen.width - 20 &&
+        if (circle.x + circle.radius >= my_screen.width - 20 &&
             circle.y + circle.radius >= rectY &&
             circle.y - circle.radius <= rectY + 40 // Altura da base retangular
         ) {
@@ -33,34 +24,23 @@ function checkCollision() {
         }
     }
 }
-
-
-function checkCollisionWithRect(circle: {
-    x: number;y: number;radius: number
-}) {
-    return (
-        circle.x + circle.radius >= my_screen.width - 20 &&
+function checkCollisionWithRect(circle) {
+    return (circle.x + circle.radius >= my_screen.width - 20 &&
         circle.y + circle.radius >= rectY &&
         circle.y - circle.radius <= rectY + 40 // Altura da base retangular
     );
 }
-
 // Função para movimentar os círculos, verificar colisões e encerrar o jogo
 function moveCircles() {
     context.clearRect(0, 0, my_screen.width, my_screen.height);
-
     drawRect(); // Desenhar a base retangular
-
     function stopGame() {
         gameRunning = false;
     }
-
     for (let i = 0; i < circles.length; i++) {
         const circle = circles[i];
         circle.x += 1; // Movimento para a direita
-
         drawCircle(circle.x, circle.y, circle.radius);
-
         if (checkCollisionWithRect(circle)) {
             circles.splice(i, 1); // Remover o círculo colidido
             i--; // Decrementar o índice para compensar a remoção do círculo
@@ -71,7 +51,8 @@ function moveCircles() {
             if (gameText) {
                 gameText.innerHTML = `Score: ${score}pts`; // Exibir a pontuação
             }
-        } else if (circle.x + circle.radius >= my_screen.width) {
+        }
+        else if (circle.x + circle.radius >= my_screen.width) {
             stopGame(); // Encerrar o jogo se o círculo atingir a base da tela sem colidir
             const gameText = document.getElementById('gameText');
             if (gameText) {
@@ -79,52 +60,44 @@ function moveCircles() {
             }
             let game_over = new Audio('sounds/game_over.mp3');
             game_over.play();
-            document.querySelector('canvas') ?.classList.add('game-over');
+            document.querySelector('canvas')?.classList.add('game-over');
             setTimeout(() => {
-                document.querySelector('canvas') ?.classList.remove('game-over');
+                document.querySelector('canvas')?.classList.remove('game-over');
             }, 200);
             return;
         }
     }
-
     if (gameRunning) {
         requestAnimationFrame(moveCircles); // Continuar movendo os círculos
     }
 }
-
 // Evento ao clicar no botão
-let my_button = document.getElementById('redraw_button') as HTMLElement;
-
+let my_button = document.getElementById('redraw_button');
 function generateRandomCircle() {
     // Gera um círculo aleatório
     const x = 0;
     const y = Math.random() * (my_screen.height - 40);
     const radius = 10;
-
     // armazena o círculo gerado
     circles.push({
         x,
         y,
         radius
     });
-
     // desenha o círculo
     drawCircle(x, y, radius);
 }
-
-function drawCircle(x: number, y: number, radius: number) {
+function drawCircle(x, y, radius) {
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
     context.fillStyle = 'White';
     context.fill();
     context.closePath();
 }
-
 my_button.onclick = function () {
     gameRunning = true;
     circles = []; // limpa os círculos
     score = 0; // zera a pontuação
-
     setInterval(() => {
         if (gameRunning) {
             generateRandomCircle();
@@ -137,15 +110,13 @@ my_button.onclick = function () {
     // começar a mover os círculos
     moveCircles();
 };
-
-
 // Eventos do teclado para mover a base retangular
 document.onkeydown = function (event) {
     var name = event.key;
-
     if (name === 'ArrowUp') {
         rectY -= 10; // Movimento para cima
-    } else if (name === 'ArrowDown') {
+    }
+    else if (name === 'ArrowDown') {
         rectY += 10; // Movimento para baixo
     }
 };
